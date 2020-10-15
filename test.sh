ac1e56393ecdfaf69f47837e6b199d1020986857
poojashetty996 <poojashetty996@gmail.com>
2020-10-15 18:47:55 +0530

    Adding script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Hello World"
+#minikube start
+#lint_template() {
+#  echo "Linting template"
+#  helm lint "/home/pooja/test_script/sample/" --debug
+#}
+#
+#lint_template
+#  if [ $? -gt 0 ]
+#  then
+#    echo "Linting failed"
+#  fi
+##helm plugin remove diff && helm plugin install https://github.com/databus23/helm-diff --version master
+#echo "Previewing changes to the helm package"
+#helm diff upgrade mysample home/pooja/test_script/sample/ --values "home/pooja/test_script/sample/values.yaml" --debug  
