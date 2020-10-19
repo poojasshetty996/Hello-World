212b9e9655ca65c1631bfec6c25d4ff44fd70300
poojashetty996 <poojashetty996@gmail.com>
2020-10-19 12:31:06 +0530

    Adding modified test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #echo "Hello World"
-echo "$HOME"
+helm lint "helm/k8s-service" --debug
 #cp -R /home/ec2-user/pooja/test/ "$PWD/." 
 #helm ls -A
 #helm lint "/home/ec2-user/gocd-server-agent/homego/test/" --debug
